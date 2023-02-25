a9fd43b367d514be257f7ad65ec1fdf840984b93
Emil Löfquist <lofquist@gmail.com>
2023-02-25 22:53:13 +0100

    Adds Actor collider for reference

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ This project is just made to show random collision detection failures.
 Move around with `A` and `D`. Hold `O` to sprint and `P` to "jump" (rather, it just moves the Actor up the Y axis for this demo purpose).
 
 While high in the air, let got of the jump button. From time to time, the Actor will fall through the floor.
+
+The yellow box is a manually added box collider. It seems to be working fine when the main actor is colliding with it from a far height.
--- src/levels/level2/index.ts
+++ src/levels/level2/index.ts
@@ -1,9 +1,23 @@
+import { Actor, CollisionType, Color, Engine, vec } from 'excalibur';
 import Maps from 'src/resources/maps';
 import BaseLevel from '../base_level';
 
 class Level2 extends BaseLevel {
   public static async build(): Promise<Level2> {
     return await super.build(Maps.map_1_2);
+  }
+
+  override onInitialize(engine: Engine): void {
+    super.onInitialize(engine);
+    const actorWithBoxCollider = new Actor({
+      pos: vec(150, engine.drawHeight - 100),
+      width: 100,
+      height: 10,
+      color: Color.Yellow,
+      collisionType: CollisionType.Fixed,
+    });
+
+    this.add(actorWithBoxCollider);
   }
 }
 
